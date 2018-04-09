0ae56364f52038a9395c2929a2ac471e19cae158
Adam Campbell <adam@hotmeteor.com>
2018-04-09 18:52:06 -0500

    Add createJob endpoint

--- src/SpurClient.php
+++ src/SpurClient.php
@@ -4,6 +4,11 @@ namespace Spur;
 
 class SpurClient extends SpurClientBase
 {
+    public function createJob(int $place_id, array $params)
+    {
+        return $this->post("places/{$place_id}/jobs", $params);
+    }
+
     public function createPlace(array $params)
     {
         return $this->post('places/register', $params);
