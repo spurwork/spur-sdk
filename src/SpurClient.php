89a0cc7e0fc488340ab8442751cc3d64251369ad
Adam Campbell <adam@hotmeteor.com>
2018-04-11 09:48:34 -0500

    Add getPositions method

--- src/SpurClient.php
+++ src/SpurClient.php
@@ -4,13 +4,24 @@ namespace Spur;
 
 class SpurClient extends SpurClientBase
 {
+    // Jobs
+
     public function createJob(int $place_id, array $params)
     {
         return $this->post("places/{$place_id}/jobs", $params);
     }
 
+    // Places
+
     public function createPlace(array $params)
     {
         return $this->post('places/register', $params);
+    }
+
+    // Positions
+
+    public function getPositions(array $queryParams = [])
+    {
+        return $this->get("positions", $queryParams);
     }
 }
