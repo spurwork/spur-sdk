e2c7483121d47325a1a20ed43ff8e151a1489645
Yaz Jallad <yazjallad@gmail.com>
2019-01-17 08:01:51 -0800

    update urls for invites from job to teams

--- src/SpurClient.php
+++ src/SpurClient.php
@@ -124,12 +124,12 @@ class SpurClient extends SpurClientBase
 
     public function createJobInvite(int $job_id, array $params)
     {
-        return $this->post("jobs/{$job_id}/job-invites", $params);
+        return $this->post("teams/{$job_id}/team-invites", $params);
     }
 
     public function createJobInvites(int $job_id, array $params)
     {
-        return $this->put("jobs/{$job_id}/job-invites", $params);
+        return $this->put("teams/{$job_id}/team-invites", $params);
     }
 
     public function deleteJobInvite(int $job_invite_id)
